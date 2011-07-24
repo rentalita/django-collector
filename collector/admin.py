98159187866839f530a52a00dca4be5782230d04
Tom Vaughan <tom@software6.net>
2011-07-24 11:43:35 -0400

    initial blob module to capture e-mail addresses

---
+++ collector/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from collector.blobs.models import Blob
+
+admin.site.register(Blob)
--- collector/models.py
+++ collector/models.py
@@ -1,3 +1,16 @@
+import random
+import string
+
 from django.db import models
 
-# Create your models here.
+class UID:
+
+        length = 40
+
+        @classmethod
+        def generate(klass):
+                return ''.join(random.choice(string.ascii_letters + string.digits) for x in range(klass.length))
+
+class Blob(models.Model):
+        uid = models.CharField(default=UID.generate, max_length=UID.length)
+        email = models.EmailField(max_length=128)
