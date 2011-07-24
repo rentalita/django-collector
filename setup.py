68af7ce528dcc58ddf0b0b3c7e953aa881996a28
Tom Vaughan <tom@software6.net>
2011-07-24 19:30:37 -0400

    use emacs to indent-region

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from distutils.core import setup
 
 setup(name='collector',
-        packages=['collector', 'collector.utils'],
-        version='0.0.1',
-        url='http://software6.net',
+      packages=['collector', 'collector.utils'],
+      version='0.0.1',
+      url='http://software6.net',
 )
 
 # Local Variables:
