90347f4a53ca9d67167d60f5cc7b53c5cbff95f1
Tom Vaughan <tom@software6.net>
2011-07-24 19:08:31 -0400

    allow default list of characters to be set too

--- collector/utils/uid.py
+++ collector/utils/uid.py
@@ -2,9 +2,10 @@ import random
 import string
 
 length = 40
+characters = string.ascii_letters + string.digits
 
 def generate():
-        return ''.join(random.choice(string.ascii_letters + string.digits) for x in range(length))
+        return ''.join(random.choice(characters) for x in range(length))
 
 # Local Variables:
 # indent-tabs-mode: nil
