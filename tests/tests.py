20b7b3d4507fca789067537684b7f93612f5070b
Tom Vaughan <tom@software6.net>
2011-07-24 19:11:14 -0400

    test setting both length and characters in collector.utils.uid

--- tests/tests.py
+++ tests/tests.py
@@ -1,8 +1,53 @@
 import collector.utils.uid as UID
 
-def test_uid():
+def __uid(length, characters):
+        __length = UID.length
+        __characters = UID.characters
+
+        UID.length = length
+        UID.characters = characters
+
         uid = UID.generate()
+
+        UID.length = __length
+        UID.characters = __characters
+
+        return uid
+
+def test_uid():
+        __length = UID.length
+        __characters = UID.characters
+
+        uid = __uid(UID.length, UID.characters)
+
         assert len(uid) == UID.length
+
+        for x in uid:
+                assert x in UID.characters
+
+        uid = __uid(128, UID.characters)
+
+        assert len(uid) == 128
+
+        for x in uid:
+                assert x in UID.characters
+
+        uid = __uid(UID.length, 'xX')
+
+        assert len(uid) == UID.length
+
+        for x in uid:
+                assert x in 'xX'
+
+        uid = __uid(128, 'xX')
+
+        assert len(uid) == 128
+
+        for x in uid:
+                assert x in 'xX'
+
+        assert __length == UID.length
+        assert __characters == UID.characters
 
 # Local Variables:
 # indent-tabs-mode: nil
