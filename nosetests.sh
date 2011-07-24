353eda4b9210a8475c787a00857157d15e5b1502
Tom Vaughan <tom@software6.net>
2011-07-24 13:16:57 -0400

    initial attempt at unit tests using nose

---
+++ nosetests.sh
@@ -0,0 +1,12 @@
+#!/bin/sh -e
+
+COLLECTOR_HOME="$(dirname $0)"
+export COLLECTOR_HOME
+
+PYTHONPATH="${COLLECTOR_HOME}":"${PYTHONPATH}"
+export PYTHONPATH
+
+DJANGO_SETTINGS_MODULE=tests.settings
+export DJANGO_SETTINGS_MODULE
+
+nosetests "$@"
---
+++ tests/tests.py
@@ -0,0 +1,5 @@
+import collector.utils.uid as UID
+
+def test_uid():
+        uid = UID.generate()
+        assert len(len) == UID.length
