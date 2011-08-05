e2a13b1bc735bbbd7c547643b144b95a733f9c20
Tom Vaughan <tom@software6.net>
2011-08-05 10:59:58 -0400

    oops

--- nosetests.sh
+++ nosetests.sh
@@ -9,6 +9,8 @@ export PYTHONPATH
 DJANGO_SETTINGS_MODULE=tests.settings
 export DJANGO_SETTINGS_MODULE
 
+cd "${COLLECTOR_HOME}"
+
 django-admin syncdb -v 0
 
 nosetests --with-coverage --cover-erase --cover-html --cover-package=collector "$@"
