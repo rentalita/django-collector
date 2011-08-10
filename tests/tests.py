de657cffdae7f188e5715d1ad652236cdea207ff
Tom Vaughan <tom@software6.net>
2011-08-10 18:30:43 -0400

    pep8 clean-ups

--- tests/tests.py
+++ tests/tests.py
@@ -15,181 +15,181 @@ uid_default_string = string.ascii_letters + string.digits
 
 
 def test_uid_defaults():
-        assert UID.get_default_length() == \
-            uid_default_length
+    assert UID.get_default_length() == \
+        uid_default_length
 
-        assert UID.get_default_string() == \
-            uid_default_string
+    assert UID.get_default_string() == \
+        uid_default_string
 
 
 def test_uid_generate():
-        uid = UID.generate()
-        assert len(uid) == uid_default_length
-        for x in uid:
-                assert x in uid_default_string
+    uid = UID.generate()
+    assert len(uid) == uid_default_length
+    for x in uid:
+        assert x in uid_default_string
 
-        uid = UID.generate(length=128)
-        assert len(uid) == 128
-        for x in uid:
-                assert x in uid_default_string
+    uid = UID.generate(length=128)
+    assert len(uid) == 128
+    for x in uid:
+        assert x in uid_default_string
 
-        uid = UID.generate(string='xYz')
-        assert len(uid) == uid_default_length
-        for x in uid:
-                assert x in 'xYz'
+    uid = UID.generate(string='xYz')
+    assert len(uid) == uid_default_length
+    for x in uid:
+        assert x in 'xYz'
 
-        uid = UID.generate(length=128, string='xYz')
-        assert len(uid) == 128
-        for x in uid:
-                assert x in 'xYz'
+    uid = UID.generate(length=128, string='xYz')
+    assert len(uid) == 128
+    for x in uid:
+        assert x in 'xYz'
 
 
 def test_uid_is_unique():
-        count = {}
+    count = {}
 
-        for uid in [UID.generate() for i in range(1, 100)]:
-                count[uid] = count.get(uid, 0) + 1
+    for uid in [UID.generate() for i in range(1, 100)]:
+        count[uid] = count.get(uid, 0) + 1
 
-        for uid in count:
-                assert count[uid] == 1
+    for uid in count:
+        assert count[uid] == 1
 
 
 def test_json_response():
-        response = JSONResponse201({'answer': 42})
-        assert response.status == 201
-        content = json.loads(response.content)
-        assert content['answer'] == 42
+    response = JSONResponse201({'answer': 42})
+    assert response.status == 201
+    content = json.loads(response.content)
+    assert content['answer'] == 42
 
 
 def __blob_model(blob, email):
-        blob = Blob.objects.get(email=email)
+    blob = Blob.objects.get(email=email)
 
-        assert blob.email == email
-        assert blob.email == str(blob)
+    assert blob.email == email
+    assert blob.email == str(blob)
 
-        blob.delete()
-        Blob.objects.get(email=email)
+    blob.delete()
+    Blob.objects.get(email=email)
 
 
 @nose.tools.raises(Blob.DoesNotExist)
 def test_blob_model():
-        email = 'example@example.com'
+    email = 'example@example.com'
 
-        blob = Blob()
+    blob = Blob()
 
-        assert len(blob.uid) > 0
-        assert blob.email == ''
+    assert len(blob.uid) > 0
+    assert blob.email == ''
 
-        blob.email = email
-        blob.save()
+    blob.email = email
+    blob.save()
 
-        content = blob.to_json()
-        assert content['email'] == email
+    content = blob.to_json()
+    assert content['email'] == email
 
-        __blob_model(blob, email)
+    __blob_model(blob, email)
 
 
 @nose.tools.raises(django.db.IntegrityError)
 def test_blob_model_uid_is_unique():
-        blob1 = Blob()
-        blob2 = Blob()
+    blob1 = Blob()
+    blob2 = Blob()
 
-        blob1.uid = blob2.uid = UID.generate()
+    blob1.uid = blob2.uid = UID.generate()
 
-        blob1.save()
-        blob2.save()
+    blob1.save()
+    blob2.save()
 
 
 @nose.tools.raises(Blob.DoesNotExist)
 def test_create_view():
-        client = Client()
+    client = Client()
 
-        email = 'example@example.com'
+    email = 'example@example.com'
 
-        # Created
-        rc = client.post('/collector/', {'collectorEmail': email})
-        assert rc.status_code == 201
+    # Created
+    rc = client.post('/collector/', {'collectorEmail': email})
+    assert rc.status_code == 201
 
-        blob = Blob.objects.get(email=email)
+    blob = Blob.objects.get(email=email)
 
-        __blob_model(blob, email)
+    __blob_model(blob, email)
 
 
 def test_create_view_errors():
-        client = Client()
+    client = Client()
 
-        # Moved Permanently
-        rc = client.get('/collector')
-        assert rc.status_code == 301
+    # Moved Permanently
+    rc = client.get('/collector')
+    assert rc.status_code == 301
 
-        # Method Not Allowed
-        rc = client.get('/collector/')
-        assert rc.status_code == 405
+    # Method Not Allowed
+    rc = client.get('/collector/')
+    assert rc.status_code == 405
 
-        # Moved Permanently
-        rc = client.post('/collector')
-        assert rc.status_code == 301
+    # Moved Permanently
+    rc = client.post('/collector')
+    assert rc.status_code == 301
 
-        # Bad Request
-        rc = client.post('/collector/')
-        assert rc.status_code == 400
+    # Bad Request
+    rc = client.post('/collector/')
+    assert rc.status_code == 400
 
-        # Bad Request
-        rc = client.post('/collector/',
-                         {'collectorEmail': 'example example.com'})
-        assert rc.status_code == 400
+    # Bad Request
+    rc = client.post('/collector/',
+                     {'collectorEmail': 'example example.com'})
+    assert rc.status_code == 400
 
-        # Bad Request
-        rc = client.post('/collector/',
-                         {'collectorEmail': 'example@example com'})
-        assert rc.status_code == 400
+    # Bad Request
+    rc = client.post('/collector/',
+                     {'collectorEmail': 'example@example com'})
+    assert rc.status_code == 400
 
-        # Bad Request
-        rc = client.post('/collector/',
-                         {'collectorEmail': 'example example com'})
-        assert rc.status_code == 400
+    # Bad Request
+    rc = client.post('/collector/',
+                     {'collectorEmail': 'example example com'})
+    assert rc.status_code == 400
 
 
 @nose.tools.raises(Blob.DoesNotExist)
 def test_delete_view():
-        client = Client()
+    client = Client()
 
-        email = 'example@example.com'
+    email = 'example@example.com'
 
-        blob = Blob()
-        blob.email = email
-        blob.uid = 'xYz'
-        blob.save()
+    blob = Blob()
+    blob.email = email
+    blob.uid = 'xYz'
+    blob.save()
 
-        # No Content
-        rc = client.get('/collector/xYz/')
-        assert rc.status_code == 204
+    # No Content
+    rc = client.get('/collector/xYz/')
+    assert rc.status_code == 204
 
-        # Not Found
-        rc = client.get('/collector/xYz/')
-        assert rc.status_code == 404
+    # Not Found
+    rc = client.get('/collector/xYz/')
+    assert rc.status_code == 404
 
-        Blob.objects.get(email=email)
+    Blob.objects.get(email=email)
 
 
 def test_delete_view_errors():
-        client = Client()
+    client = Client()
 
-        # Moved Permanently
-        rc = client.post('/collector/xYz')
-        assert rc.status_code == 301
+    # Moved Permanently
+    rc = client.post('/collector/xYz')
+    assert rc.status_code == 301
 
-        # Method Not Allowed
-        rc = client.post('/collector/xYz/')
-        assert rc.status_code == 405
+    # Method Not Allowed
+    rc = client.post('/collector/xYz/')
+    assert rc.status_code == 405
 
-        # Moved Permanently
-        rc = client.get('/collector/xYz')
-        assert rc.status_code == 301
+    # Moved Permanently
+    rc = client.get('/collector/xYz')
+    assert rc.status_code == 301
 
-        # Not Found
-        rc = client.get('/collector/xYz/')
-        assert rc.status_code == 404
+    # Not Found
+    rc = client.get('/collector/xYz/')
+    assert rc.status_code == 404
 
 
 # Local Variables:
