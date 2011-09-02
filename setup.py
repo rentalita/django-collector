3491e56b3ccb06763d7897360f24cc069ada1dd0
Tom Vaughan <tom@software6.net>
2011-09-01 21:47:43 -0300

    as required to host releases on http://pypi.python.org/pypi

--- setup.py
+++ setup.py
@@ -2,8 +2,13 @@
 
 from setuptools import setup, find_packages
 
-setup(name='collector', version='2.0.0',
-      packages=find_packages('src/python'), package_dir={'': 'src/python'})
+setup(name='collector', version='2.0.1',
+      packages=find_packages('src/python'), package_dir={'': 'src/python'},
+      author='Software 6',
+      author_email='hello@software6.net',
+      description='Collect e-mail addresses to tell people when a start-up has launced.',
+      url='https://github.com/software6/django-collector',
+)
 
 # Local Variables:
 # indent-tabs-mode: nil
