b16665d03ec501e3e54b28b21ee5b146fa8c75b4
Tom Vaughan <tom@software6.net>
2011-09-22 16:37:53 -0300

    a more sensible name when uploaded to pypi

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 from setuptools import setup, find_packages
 
-setup(name='collector', version='2.0.0',
+setup(name='django-collector', version='2.0.0',
       packages=find_packages('src/python'), package_dir={'': 'src/python'},
       author='Software 6',
       author_email='hello@software6.net',
