0747b95d95011fe9631934fd22a1acfd4bc16dac
Tom Vaughan <tom@software6.net>
2011-09-22 16:16:34 -0300

    oops

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(name='collector', version='2.0.1',
       packages=find_packages('src/python'), package_dir={'': 'src/python'},
       author='Software 6',
       author_email='hello@software6.net',
-      description='Collect e-mail addresses to tell people when a start-up has launced.',
+      description='Collect email addresses to tell people when a start-up has launched.',
       url='https://github.com/software6/django-collector',
 )
 
