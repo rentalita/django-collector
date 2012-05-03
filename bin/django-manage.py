504dfde80eb4f2b25b429ca37bc6d1f6b7e947a2
Tom Vaughan <tom@software6.net>
2012-05-03 17:04:00 -0400

    per newer django versions

--- bin/django-manage.py
+++ bin/django-manage.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 
-from collector.manage import *
+import sys
 
-execute_manager(settings)
+from django.core.management import execute_from_command_line
+
+execute_from_command_line(sys.argv)
 
 # Local Variables:
 # indent-tabs-mode: nil
